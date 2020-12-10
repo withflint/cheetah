e0e087a55cf157c40632d077cbfaaa2e6a128244
pre63 <pre63@users.noreply.github.com>
2020-12-09 21:18:14 -0800

    bash substitution?

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build . -t cheetah:$GIT_VERSION --build-arg GIT_VERSION=$GIT_VERSION
-docker tag cheetah:$GIT_VERSION 717566463867.dkr.ecr.ca-central-1.amazonaws.com/cheetah:$GIT_VERSION
-docker push 717566463867.dkr.ecr.ca-central-1.amazonaws.com/cheetah:$GIT_VERSION
+docker build . -t "cheetah:$GIT_VERSION" --build-arg "GIT_VERSION=$GIT_VERSION"
+docker tag cheetah:$GIT_VERSION "717566463867.dkr.ecr.ca-central-1.amazonaws.com/cheetah:$GIT_VERSION"
+docker push "717566463867.dkr.ecr.ca-central-1.amazonaws.com/cheetah:$GIT_VERSION"
 echo "- published cheetah:$GIT_VERSION" >> output
